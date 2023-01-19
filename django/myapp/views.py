96e2e75196235fad3a7e432847c74589448a92c8
blueconecell <95961799+blueconecell@users.noreply.github.com>
2023-01-19 17:42:18 +0900

    django end

--- django/myapp/views.py
+++ django/myapp/views.py
@@ -6,9 +6,19 @@ topics = [
     {'id':2, 'title':'view', 'body':'View is ..'},
     {'id':3, 'title':'Model', 'body':'Model is ..'},
 ]
-def HTMLTemplate(articleTag):
+def HTMLTemplate(articleTag, id=None):
     global topics
-
+    contextUI=''
+    if id != None:
+        contextUI = f'''
+            <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
+            <li><a href="/update/{id}">update</a></li>
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
@@ -22,6 +32,7 @@ def HTMLTemplate(articleTag):
         {articleTag}
         <ul>
             <li><a href="/create/">create</a></li>
+            {contextUI}
         </ul>
     </body>
     </html>
@@ -39,7 +50,7 @@ def read(request, id):
     for topic in topics:
         if topic['id']==int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article,id))
 
 @csrf_exempt
 def create(request):
@@ -61,3 +72,42 @@ def create(request):
         url = '/read/'+str(nextId)
         nextId+=1
         return redirect(url)
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == 'POST':
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    "title":topic['title'],
+                    "body":topic['body']
+                }
+        article = f'''
+            <form action='/update/{id}/' method='post'>
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article,id))
+    
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title']=title
+                topic['body']=body
+        return redirect(f'/read/{id}')
+    
