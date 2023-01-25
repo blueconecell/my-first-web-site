49331211b3339fd7a473b09ac24d13b954d0b4e9
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-01-25 18:42:51 +0900

    update complete

--- nodejs/main.js
+++ nodejs/main.js
@@ -10,8 +10,15 @@ function templateHTML(title, list, body, control) {
   <head>
     <title>WEB1 - ${title}</title>
     <meta charset="utf-8">
+    <style>
+      textarea {
+        width: 400px;
+			  height: 200px;
+      }
+    </style>
   </head>
   <body>
+    
     <h1><a href="/">WEB</a></h1>
     ${list}
     ${control}
@@ -24,7 +31,7 @@ function checkTitleForm(titleName) {
   if (!parseInt(titleName.substring(0, 1))) {
     return titleName;
   } else {
-    return titleName.substring(3);
+    return titleName.substring(2);
   }
 }
 function templateList(filelist) {
@@ -84,7 +91,7 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-      <form action="http://localhost:3000/create_process" method="post">
+      <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder='title'/></p>
         <p><textarea name="description" placeholder='description'></textarea></p>
         <p><input type="submit" /></p>
@@ -110,6 +117,48 @@ var app = http.createServer(function (request, response) {
       fs.writeFile(`data/${title}`, description, "utf8", function (err) {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end("success");
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./data", function (error, filelist) {
+      console.log(filelist);
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var title = queryData.id;
+        console.log("querdyData.id : " + queryData.id);
+        var list = templateList(filelist);
+        console.log(parseInt(title.substring(0, 1)));
+        var template = templateHTML(
+          title,
+          list,
+          `
+          <form action="update_process" method="post">
+            <input type='hidden' name='id' value='${title}'>
+            <p><input type="text" name="title" placeholder='title' value=${title}></p>
+            <p><textarea name="description" placeholder='description'>${description}</textarea></p>
+            <p><input type="submit" /></p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if (pathname === "/update_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body += data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end("success");
+        });
       });
     });
   } else {
